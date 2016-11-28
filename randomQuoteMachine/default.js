49930a91361153ce755deac0d6aab0aee0f7cf5b
gp22 <gp22@tutanota.com>
2016-11-28 13:17:42 -0800

    remove jQuery, start work on XMLHttpRequest function

--- randomQuoteMachine/default.js
+++ randomQuoteMachine/default.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
-        $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
-      });
+
+function getQuote() {
+  var request = new XMLHttpRequest();
+  var proxy = 'http://crossorigin.me/';
+  var api = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
+  var url = proxy + api;
+
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = this.responseXML;
+      return response;
+    }
+  }
+
+  request.open('GET', url, true);
+  request.send();
+}
