9c9cc0b6ce61adb591c544a913175db68a88e3bf
gp22 <gp22@tutanota.com>
2016-12-02 20:11:11 -0800

    final getQuote function and styling

--- randomQuoteMachine/default.js
+++ randomQuoteMachine/default.js
@@ -1,19 +1,19 @@
-
 function getQuote() {
-  var xhr = new XMLHttpRequest();
-  var proxy = 'http://crossorigin.me/';
-  var api = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
-  var url = proxy + api;
+    var xhr = new XMLHttpRequest();
+    // api website for documentation http://quotes.stormconsultancy.co.uk/api
+    var url = 'http://quotes.stormconsultancy.co.uk/random.json';
 
-  xhr.open('GET', url, true);
-  xhr.send();
-  xhr.onreadystatechange = processRequest;
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest;
 
-  function processRequest(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var response = xhr.response;
-      // console.log(response);
-      return xhr;
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            document.getElementById('quoteText').innerHTML = response.quote;
+            document.getElementById('quoteAuthor').innerHTML = response.author;
+        }
     }
-  }
 }
+
+getQuote();
