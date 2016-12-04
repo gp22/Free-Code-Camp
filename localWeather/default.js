97dbdba2ca4a5d781d0291515efacd8dcf51f47b
gp22 <gp22@tutanota.com>
2016-12-04 15:29:05 -0800

    add function definitions

--- localWeather/default.js
+++ localWeather/default.js
@@ -1,4 +1,3 @@
-
 function geoLocate() {
     var lat = 0;
     var lon = 0;
@@ -17,8 +16,8 @@ function geoLocate() {
 function getForecast(lat, lon) {
     var xhr = new XMLHttpRequest();
     var api = 'http://api.openweathermap.org/data/2.5/weather?';
-    var key = '&APPID=0abcae66974746b7b7bf53438db71f24';
-    var apiUrl = api + 'lat=' + lat + '&lon=' + lon + key;
+    var key = '0abcae66974746b7b7bf53438db71f24';
+    var apiUrl = api + 'lat=' + lat + '&lon=' + lon + '&APPID=' + key;
 
     xhr.open('GET', apiUrl, true);
     xhr.send();
@@ -27,36 +26,42 @@ function getForecast(lat, lon) {
     function processRequest() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
-            // var tempFahrenheit = kelvinToFahrenheit(response.main.temp);
-            var windSpeedMph = mpsToMph(response.wind.speed);
 
-            // document.getElementById('quoteText').innerHTML = response.quote;
-            // document.getElementById('quoteAuthor').innerHTML = response.author;
-            // document.getElementById('tempFahrenheit').innerHTML =
-            // document.getElementById('description').innerHTML = response.weather[0].description;
-            // document.getElementById('wind').innerHTML = response.wind.speed;
-            // document.getElementById('humidity').innerHTML = response.main.humidity;
+            document.getElementById('temp_max').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp_max));
+            document.getElementById('temp_min').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp_min));
+            document.getElementById('temp').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp));
+            document.getElementById('description').innerHTML = response.weather[0].main;
+            document.getElementById('wind').innerHTML = Math.round(metersPsToMilesPh(response.wind.speed));
+            document.getElementById('humidity').innerHTML = response.main.humidity;
 
-            console.log(response.weather[0].description);
-            console.log(response.wind.speed);
-            console.log(response.main.humidity);
+            // console.log(response.weather[0].main);
+            // console.log(response.wind.speed);
+            // console.log(response.main.humidity);
             console.log(response);
         }
     }
 }
 
 function kelvinToFahrenheit(tempKelvin) {
-
+  // convert kelvin to fahrenheit
+  var tempFahrenheit = tempKelvin * 9/5 - 459.67;
+  return tempFahrenheit;
 }
 
 function fahrenheitToCelsius(tempFahrenheit) {
-
+  // convert fahrenheit to celsius
+  var tempCelsius = (tempFahrenheit - 32) * 5/9
+  return tempCelsius;
 }
 
 function celsiusToFahrenheit(tempCelsius) {
-
+  // convert celsius to fahrenheit
+  var tempFahrenheit = tempCelsius * 9/5 + 32;
+  return tempFahrenheit;
 }
 
-function mpsToMph(metersPerSecond) {
-
+function metersPsToMilesPh(metersPs) {
+  // convert meters per second to miles per hour
+  var milesPh = metersPs * 9/4;
+  return milesPh;
 }
