9a06f3fcba4db912c6d7dd21d4eb0aa47a63d919
gp22 <gp22@tutanota.com>
2016-12-05 16:45:37 -0800

    add functions to convert units

--- localWeather/default.js
+++ localWeather/default.js
@@ -27,41 +27,108 @@ function getForecast(lat, lon) {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
 
-            document.getElementById('temp_max').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp_max));
-            document.getElementById('temp_min').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp_min));
-            document.getElementById('temp').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp));
+            document.getElementById('temp_max').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp_max)) + ' F';
+            document.getElementById('temp_min').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp_min)) + ' F';
+            document.getElementById('temp').innerHTML = Math.round(kelvinToFahrenheit(response.main.temp)) + ' F';
             document.getElementById('description').innerHTML = response.weather[0].main;
-            document.getElementById('wind').innerHTML = Math.round(metersPsToMilesPh(response.wind.speed));
+            document.getElementById('wind').innerHTML = Math.round(metersPsToMilesPh(response.wind.speed)) + ' MPH';
             document.getElementById('humidity').innerHTML = response.main.humidity;
 
-            // console.log(response.weather[0].main);
-            // console.log(response.wind.speed);
-            // console.log(response.main.humidity);
             console.log(response);
         }
     }
 }
 
 function kelvinToFahrenheit(tempKelvin) {
-  // convert kelvin to fahrenheit
-  var tempFahrenheit = tempKelvin * 9/5 - 459.67;
-  return tempFahrenheit;
+    // convert kelvin to fahrenheit
+    var tempFahrenheit = tempKelvin * 9 / 5 - 459.67;
+    return tempFahrenheit;
 }
 
 function fahrenheitToCelsius(tempFahrenheit) {
-  // convert fahrenheit to celsius
-  var tempCelsius = (tempFahrenheit - 32) * 5/9
-  return tempCelsius;
+    // convert fahrenheit to celsius
+    var tempCelsius = (tempFahrenheit - 32) * 5 / 9
+    return tempCelsius;
 }
 
 function celsiusToFahrenheit(tempCelsius) {
-  // convert celsius to fahrenheit
-  var tempFahrenheit = tempCelsius * 9/5 + 32;
-  return tempFahrenheit;
+    // convert celsius to fahrenheit
+    var tempFahrenheit = tempCelsius * 9 / 5 + 32;
+    return tempFahrenheit;
 }
 
 function metersPsToMilesPh(metersPs) {
-  // convert meters per second to miles per hour
-  var milesPh = metersPs * 9/4;
-  return milesPh;
+    // convert meters per second to miles per hour
+    var milesPh = metersPs * 9 / 4;
+    return milesPh;
 }
+
+function milesToKilometers(miles) {
+    // convert miles to kilometers
+    return miles * 1.6;
+}
+
+function kilometersToMiles(kilometers) {
+    // convert kilometers to miles
+    return kilometers * 0.62;
+}
+
+function changeUnits() {
+    // check if units are imperial, if they are, change to metric
+    if (document.getElementById('temp').innerHTML.slice(-1) === 'F') {
+        // change current temperature
+        var tempFahrenheit = parseInt(document.getElementById('temp').innerHTML.split(' ')[0], 10);
+        var tempCelsius = Math.round(fahrenheitToCelsius(tempFahrenheit));
+        tempCelsius.toString(10);
+        document.getElementById('temp').innerHTML = tempCelsius + ' C';
+
+        // change high temp
+        tempFahrenheit = parseInt(document.getElementById('temp_max').innerHTML.split(' ')[0], 10);
+        tempCelsius = Math.round(fahrenheitToCelsius(tempFahrenheit));
+        tempCelsius.toString(10);
+        document.getElementById('temp_max').innerHTML = tempCelsius + ' C';
+
+        // change low temp
+        tempFahrenheit = parseInt(document.getElementById('temp_min').innerHTML.split(' ')[0], 10);
+        tempCelsius = Math.round(fahrenheitToCelsius(tempFahrenheit));
+        tempCelsius.toString(10);
+        document.getElementById('temp_min').innerHTML = tempCelsius + ' C';
+
+        // change wind speed
+        var windSpeedMiles = parseInt(document.getElementById('wind').innerHTML.split(' ')[0], 10);
+        var windSpeedKilos = Math.round(milesToKilometers(windSpeedMiles));
+        windSpeedKilos.toString(10);
+        document.getElementById('wind').innerHTML = windSpeedKilos + ' KPH';
+
+    } else { // else, change to imperial
+        // change current temperature
+        var tempCelsius = parseInt(document.getElementById('temp').innerHTML.split(' ')[0], 10);
+        var tempFahrenheit = Math.round(celsiusToFahrenheit(tempCelsius));
+        tempCelsius.toString(10);
+        document.getElementById('temp').innerHTML = tempFahrenheit + ' F';
+
+        // change high temp
+        var tempCelsius = parseInt(document.getElementById('temp_max').innerHTML.split(' ')[0], 10);
+        var tempFahrenheit = Math.round(celsiusToFahrenheit(tempCelsius));
+        tempCelsius.toString(10);
+        document.getElementById('temp_max').innerHTML = tempFahrenheit + ' F';
+
+        // change low temp
+        var tempCelsius = parseInt(document.getElementById('temp_min').innerHTML.split(' ')[0], 10);
+        var tempFahrenheit = Math.round(celsiusToFahrenheit(tempCelsius));
+        tempCelsius.toString(10);
+        document.getElementById('temp_min').innerHTML = tempFahrenheit + ' F';
+
+        // change wind speed
+        var windSpeedKilos = parseInt(document.getElementById('wind').innerHTML.split(' ')[0], 10);
+        var windSpeedMiles = Math.round(kilometersToMiles(windSpeedKilos));
+        windSpeedMiles.toString(10);
+        document.getElementById('wind').innerHTML = windSpeedMiles + ' MPH';
+    }
+}
+
+var geoLocateButton = document.getElementById('geoLocateButton');
+var changeUnitsButton = document.getElementById('changeUnits');
+
+geoLocateButton.addEventListener('click', geoLocate, false);
+changeUnitsButton.addEventListener('click', changeUnits, false);
